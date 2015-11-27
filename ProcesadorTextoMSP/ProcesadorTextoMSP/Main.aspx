44edc8833d47a4a0b646b3415cbfc709408ac33c
bgLeon <borruellabad@gmail.com>
2015-11-27 13:59:14 +0100

    Implementada separación por espacios, modificado el layout de salida para que muestre todas las letras con un mismo tamaño

--- ProcesadorTextoMSP/ProcesadorTextoMSP/Main.aspx
+++ ProcesadorTextoMSP/ProcesadorTextoMSP/Main.aspx
@@ -1,40 +1,42 @@
-﻿<%@ Page Title= "Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="ProcesadorTextoMSP.Main" %>
+﻿<%@ Page Title="Home Page" Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="ProcesadorTextoMSP.Main" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
+
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
-       Procesador de Texto MSP<br />
-        <br />
-        <br />
-        Introduce el numero máximo de caracteres por linea:<br />
-        <asp:TextBox ID="Columnas" runat="server" Width="29px" Height="19px"></asp:TextBox>
-        <br />
-        <br />
-        Introduce aquí tu texto:<br />
-        <asp:TextBox ID="Texto" runat="server" Height="22px" Width="742px"></asp:TextBox>
-        <asp:Button ID="ProcButton" runat="server" OnClick="ProcButton_Click" Text="Procesar" Width="144px" ForeColor="Blue" />
-        <br />
-        <br />
-        <asp:Label ID="TextoProcesadoLabel" runat="server" Text=""></asp:Label>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-    
-    </div>
+        <div>
+            Procesador de Texto MSP<br />
+            <br />
+            <br />
+            Introduce el numero máximo de caracteres por linea:<br />
+            <asp:TextBox ID="Columnas" runat="server" Width="29px" Height="19px"></asp:TextBox>
+            <br />
+            <br />
+            Introduce aquí tu texto:<br />
+            <asp:TextBox ID="Texto" runat="server" Height="22px" Width="742px"></asp:TextBox>
+            <asp:Button ID="ProcButton" runat="server" OnClick="ProcButton_Click" Text="Procesar" Width="144px" ForeColor="Blue" />
+            <br />
+            <br />
+            <pre>
+                 <asp:Label ID="TextoProcesadoLabel" runat="server"></asp:Label>
+            </pre>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+        </div>
     </form>
 </body>
 </html>
--- ProcesadorTextoMSP/ProcesadorTextoMSP/Main.aspx.cs
+++ ProcesadorTextoMSP/ProcesadorTextoMSP/Main.aspx.cs
@@ -9,14 +9,89 @@ namespace ProcesadorTextoMSP
 {
     public partial class Main : System.Web.UI.Page
     {
+        private  static HashSet<Char> vocales;
         protected void Page_Load(object sender, EventArgs e)
         {
+            vocales = new HashSet<Char> { 'a', 'e', 'i', 'o' , 'u'};
+        }
+        private static String ProcesaTexto(String texto, int lineas)// procesa el texto
+        {
+            String exit = "";
+            String nextLine = "";
+            String posLine = ""; // salida con el ultimo espacio
+            int antI = 0; // parametro para recordar donde se encontro el ultimo espacio
+            int c = 0;
+            
+            Boolean espacio = false;
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i] == ' ')
+                {
+                    posLine = nextLine;
+                    espacio = true;
+                    antI = i;
+                }
+                nextLine += texto[i];
+                c++;
+                if (c == 1 && texto[i] == ' ') //para que no se empiece una linea con un espacio en el caso de que la linea anterior acabara con una palabra completa
+                {
+                    nextLine = "";
+                    c--;
+                }
+                if (c == lineas || i == texto.Length - 1)// si se llega al límite o se acaba el texto
+                {
+                    if (i < texto.Length - 1)
+                    {
+                        if (texto[i + 1] == ' ') espacio = false; // si se ha alcanzado el final de una palabra no hace falta volver al espacio anterior
+                    }
+                    if (espacio == true && i != texto.Length - 1)
+                    {
+                        nextLine = posLine;
+                        i = antI;
+                    }
+                    if (nextLine.Length > 0)// para evitar escribir lineas con un espacio
+                    {
+                        exit += "<br />" + nextLine;
+                    }
+                    c = 0;
+                    espacio = false;
+                    nextLine = "";
+                }
+            }
+            return exit;
+        }
 
+
+        private static String ProcesaSilabas(int columnas, String nextline, int espacio, String nextChars)
+        {
+            int end = nextline.Length -1;
+            if (vocales.Contains(nextline[end])){
+                if (vocales.Contains(nextChars[0]))
+                {
+                    return nextline;
+                }
+            }
+                
+            
+            //vocal + vocal -> separa
+            //vocal + consonante + consonante si no es rr o ll o ch separa despues de la primera cons
+            //vocal + conssonante + consonante || + consonante  pero si la tercera es una r o una l separar rn la primera
+
+
+            return "";
         }
 
         protected void ProcButton_Click(object sender, EventArgs e)
         {
-            TextoProcesadoLabel.Text = Texto.Text + " Cada vez estas mas cerca";
+             
+            String texto = Texto.Text;
+            int columnas;
+            if (Int32.TryParse(Columnas.Text, out columnas));
+            else {
+                String message = Columnas.Text + " no es un número";
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + message + "');", true);
+            }
+            TextoProcesadoLabel.Text = ProcesaTexto(texto, columnas);
         }
     }
 }
